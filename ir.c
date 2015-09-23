18c5771c3ff6dabcfba630bedc54c1e6639eb29f
Fabian van der Werf <fvanderwerf@gmail.com>
2015-09-23 21:07:15 +0200

    cleanup

--- ir.c
+++ ir.c
@@ -8,12 +8,26 @@
 
 #define PULSES_PER_SYMBOL 32
 
+#define IR_CLOCK_DIV (_BV(CS12))
+
+#define IR_ENABLE_CLOCK()       \
+    do {                        \
+        TCCR1 |= IR_CLOCK_DIV;  \
+    } while(0)
+
+#define IR_DISABLE_CLOCK()      \
+    do {                        \
+        TCCR1 &= 0xF0;          \
+    } while(0)
+
+
 static enum ir_state {
     IR_NONE,
     IR_INIT,
     IR_SEND,
     IR_DONE
 } ir_state = IR_NONE;
+
 
 // RC-5 TV '0'
 static int ir_pattern_ocra[]   = {  0, 60,  0, 60, 60,  0, 60,  0, 60,  0, 60,  0, 60,  0, 60,  0, 60,  0, 60,  0, 60,  0, 60,  0, 60,  0, 60,  0 };
@@ -37,7 +51,7 @@ inline void init_timer1(void)
     PLLCSR |= _BV(PCKE); // set PLL as source for timer 1
     TCCR1 = _BV(CTC1) | _BV(PWM1A) | _BV(COM1A1);  //clear timer1 on ocr1c match, enable pwm1a, pwm1a mode: set on 0, clear on match, clock is disabled
     OCR1A = 0;           // 0% dutycycle
-    OCR1C = 221;         // 64MHz divided by 8 = 8Mhz, compare match clear at 221, --> 8Mhz / (221 + 1) = 36.0kHz
+    OCR1C = 255;         // non zero to avoid overflow interrupt
 
     TIFR |= _BV(TOV1);   // clear overflow interrupt if there is one pending
     TIMSK |= _BV(TOIE1); // enable timer1 overflow interrupt
@@ -78,22 +92,26 @@ ISR(TIM1_OVF_vect)
         case IR_NONE:
         default:
             ir_state = IR_NONE;
-            TCCR1 &= 0xF0; //disable clock
+            IR_DISABLE_CLOCK();
             break;
     }
 }
 
-void send_code(void)
+static void ir_reset(void)
 {
+    IR_DISABLE_CLOCK();
+
     ir_state = IR_INIT;
     ir_pattern_index = 0;
 
     TCNT1 = 0;
     OCR1A = 0;
+    OCR1C = 221;          // 64MHz divided by 8 = 8Mhz, compare match clear at 221, --> 8Mhz / (221 + 1) = 36.0kHz
+
     GTCCR |= _BV(PSR1); //reset prescaler
     TIFR |= _BV(TOV1); //reset any pending interrupt
-    TCCR1 |= _BV(CS12); //enable clock
 }
+
 
 void ir_write_pattern(int value, int *offset)
 {
@@ -111,6 +129,8 @@ void ir_send(uint8_t address, uint8_t code)
     int i = 0;
     int j;
     static int toggle = 0;
+
+    ir_reset();
 
     /* start bit */
     ir_write_pattern(1, &i);
@@ -133,5 +153,6 @@ void ir_send(uint8_t address, uint8_t code)
     for (j = 0; j < 6; j++)
         ir_write_pattern((code >> (5 - j)) & 0x01, &i);
 
-    send_code();
+    IR_ENABLE_CLOCK();
 }
+
